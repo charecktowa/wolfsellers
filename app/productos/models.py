4c50e77eff7c7c76439d6858db8c94f898b0035c
Andrea Gutiérrez Alfaro <charecktowa@protonmail.com>
2025-07-23 17:13:29 -0600

    Se creó el modelo para productos

--- app/productos/models.py
+++ app/productos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Producto(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+    descripcion = models.TextField(blank=True)
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+    disponible = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.nombre

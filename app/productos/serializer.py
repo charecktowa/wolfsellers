861520288c090a361b3aa996aee66ec984b5426f
Andrea Gutiérrez Alfaro <charecktowa@protonmail.com>
2025-07-23 17:13:52 -0600

    Se añadio serialización y validación para los modelos

---
+++ app/productos/serializer.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Producto
+
+
+class ProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Producto
+        fields = "__all__"
+        read_only_fields = "id"
+
+    # Dado que 'nombre' es requerido, se hace una validación
+    def validar_nombre(self, nombre: str) -> str:
+        if not nombre:
+            raise serializers.ValidationError("El nombre es obligatorio.")
+        return nombre.strip()
+
+    def validar_precio(self, precio: float) -> float:
+        if precio <= 0:
+            raise serializers.ValidationError("El precio debe ser mayor que cero.")
+        return precio

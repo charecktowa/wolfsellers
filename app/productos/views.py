d0776b8830c95c4b068cfd929f97e7144963bef2
Andrea Gutiérrez Alfaro <charecktowa@protonmail.com>
2025-07-23 17:19:44 -0600

    Se creo vista de producto

--- app/productos/views.py
+++ app/productos/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from app.productos.models import Producto
+from app.productos.serializer import ProductoSerializer
+
 
 # Create your views here.
+class ProductoViewSet(viewsets.ModelViewSet):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
